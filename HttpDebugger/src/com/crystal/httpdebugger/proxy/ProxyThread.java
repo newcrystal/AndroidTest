0604bd70368245b5dbfaedbc00af109cfc5375a3
crystal <crystal123@naver.com>
2015-11-21 18:30:03 +0900

    read 속도 개선

--- HttpDebugger/src/com/crystal/httpdebugger/proxy/ProxyThread.java
+++ HttpDebugger/src/com/crystal/httpdebugger/proxy/ProxyThread.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 public class ProxyThread extends Thread {
     private Socket socket = null;
-    private static final int BUFFER_SIZE = 1024*2*2;
+    //private static final int BUFFER_SIZE = 1024*2*2;
     public ProxyThread(Socket socket) {
         super("ProxyThread");
         this.socket = socket;
@@ -32,8 +32,8 @@ public class ProxyThread extends Thread {
 
         	///////////////////////////////////
             //begin get request from client
-            Socket directSocket = new Socket();
-            directSocket.setSoTimeout(3000);
+            Socket realServerSocket = new Socket();
+            realServerSocket.setSoTimeout(10000);
             PrintWriter pw = null;
             while ((inputLine = in.readLine()) != null && !"".equals(inputLine)) {
                 try {
@@ -51,8 +51,8 @@ public class ProxyThread extends Thread {
                         URI uri;
         				uri = url.toURI();
         	            InetAddress address = InetAddress.getByName(uri.getHost());
-        	            directSocket.connect(new InetSocketAddress(address, 80), 5000);
-        	            pw = new PrintWriter(directSocket.getOutputStream());
+        	            realServerSocket.connect(new InetSocketAddress(address, 80), 5000);
+        	            pw = new PrintWriter(realServerSocket.getOutputStream());
                     }
                     pw.println(inputLine);
                 } catch (Exception e) {
@@ -67,28 +67,43 @@ public class ProxyThread extends Thread {
             }
             
 		    try {
-            	InputStream directInput = directSocket.getInputStream();
+		    	int buffersize = realServerSocket.getReceiveBufferSize();
+            	int BUFFER_SIZE = 1024;
+            	BufferedInputStream realServerResult = new BufferedInputStream(realServerSocket.getInputStream(), BUFFER_SIZE);
             	 
-                byte by[] = new byte[ BUFFER_SIZE ];
-                int index = directInput.read( by, 0, BUFFER_SIZE );
+                /*byte by[] = new byte[ BUFFER_SIZE ];
+                int index = realServerResult.read( by, 0, BUFFER_SIZE );
                 StringBuilder sb = new StringBuilder();
                 while ( index != -1 )
                 {
                   sb.append(new String(by, 0, index, Charset.forName("UTF-8")));
                   out.write( by, 0, index );
-                  index = directInput.read( by, 0, BUFFER_SIZE );
+                  index = realServerResult.read( by, 0, BUFFER_SIZE );
                 }
                 if (urlToCall.indexOf(".png") < 0 && urlToCall.indexOf(".gif") < 0 && urlToCall.indexOf(".jpg") < 0) {
                 	System.out.println(sb.toString());
-                }
+                }*/
+            	byte buffer[] = new byte [BUFFER_SIZE];
+            	int bytesRead = 0;
+            	StringBuilder result = new StringBuilder();
+            	
+            	while ((bytesRead = realServerResult.read(buffer, 0, BUFFER_SIZE)) != -1) {
+            		result.append(new String(buffer, 0, bytesRead));
+            		System.out.println(result);
+            		out.write(buffer, 0, bytesRead);
+            		
+            		if (bytesRead < BUFFER_SIZE) break;
+            	}
+            
+            	
              } catch (Exception e) {
                 System.err.println("Encountered exception: " + e);
                 out.writeBytes("");
             }
             out.flush();
             
-            if (directSocket != null) {
-            	directSocket.close();
+            if (realServerSocket != null) {
+            	realServerSocket.close();
             }	
             if (out != null) {
                 out.close();
