c5d8e242491672613ea89a79b6b43f5fa09dc4f0
crystal <crystal123@naver.com>
2015-11-24 23:29:10 +0900

    url, port 셋팅 수정

--- HttpDebugger/src/com/crystal/httpdebugger/proxy/domain/HttpRequest.java
+++ HttpDebugger/src/com/crystal/httpdebugger/proxy/domain/HttpRequest.java
@@ -1,12 +1,14 @@
 package com.crystal.httpdebugger.proxy.domain;
 
+import android.annotation.SuppressLint;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
 public class HttpRequest {
-	private String url;//1.[1]]
-	private String method; //1.[0]
+	private String url;
+	private String method;
+	private String protocol;
 	private int port = 80;
 	private Map<String, String> header = new HashMap<String, String>();
 	
@@ -19,11 +21,17 @@ public class HttpRequest {
 		else addHeader(inputLine.split(": ")[0], inputLine.split(": ")[1]);
 	}
 	
+	@SuppressLint("DefaultLocale")
 	public void initialize(String line) {
 		String[] tokens = line.split(" ");
         method = tokens[0];
         url = tokens[1];
+    	protocol = tokens[2].split("/[0-9]\\.?[0-9]")[0];
+        if (url.indexOf("http") < 0 && url.indexOf("HTTP") < 0){
+        	url = new StringBuilder().append(protocol.toLowerCase()).append("://").append(url).toString();
+        }
         extractPort();
+        url = url.replaceAll(":"+port,"");
 	}
 
 	private void extractPort() {
